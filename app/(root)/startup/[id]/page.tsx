02f98df3f009ff52c2a0f63515a0be69e8ad9eb4
artgustavobarros <artgustavobarros@gmail.com>
2024-11-11 18:58:58 -0300

    feat: enable expermental_ppr

---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { STARTUP_BY_ID_QUERY } from "@/lib/queries"
+import { client } from "@/sanity/lib/client"
+import { notFound } from "next/navigation"
+
+export const experimental_ppr = true
+
+export default async function Page({params}: {params: Promise<{id: string}>}){
+    const id = (await params).id
+    const post = await client.fetch(STARTUP_BY_ID_QUERY, {id})
+
+    if(!post) return notFound()
+    return (
+        <>
+            <h1>{post.title}</h1>
+        </>
+    )
+}
